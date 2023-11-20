90baf97a1cd4545a5b5d441658f2614ded20545e
Sudarshan <sudarshanvijayshanker64@gmail.com>
2023-11-19 22:03:04 -0500

    Changes

--- Hi.java
+++ Hi.java
@@ -1,5 +1,22 @@
+class Student{
+    public int rollno;
+    public String name;
+    Student(int rollno,String name){
+        this.rollno=rollno;
+        this.name=name;
+    }
+}
 public class Hi {
     public static void main(String[] args){
-        System.out.println("hi");
+        Student[] arr;
+        arr=new Student[3];
+        arr[0]=new Student(1,"amar");
+        arr[1]=new Student(2,"tom");
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println("element at"+i+":"+arr[i].rollno+" "+arr[i].name);
+        }
+
+
     }
 }
